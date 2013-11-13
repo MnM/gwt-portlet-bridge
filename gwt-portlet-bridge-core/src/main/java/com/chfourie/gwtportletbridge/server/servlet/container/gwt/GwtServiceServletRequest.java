d00ea1f95a4d255102c44361bb26d18201e7f76a
Matthias Maiershofer <matthias.maiershofer@tomaxx.com>
2013-11-13 11:30:06 +0100

    Don't double-decode the request parameters. When the server already decodes the x-www-form-urlencoded request correctly. By decoding it again, we fail to "decode" certain special characters, like % and + (with the latter being silently ignored and the former throwing an exception). Fixes #1.

--- gwt-portlet-bridge-core/src/main/java/com/chfourie/gwtportletbridge/server/servlet/container/gwt/GwtServiceServletRequest.java
+++ gwt-portlet-bridge-core/src/main/java/com/chfourie/gwtportletbridge/server/servlet/container/gwt/GwtServiceServletRequest.java
@@ -2,21 +2,19 @@ package com.chfourie.gwtportletbridge.server.servlet.container.gwt;
 
 import com.chfourie.gwtportletbridge.server.servlet.container.HttpServletRequestImpl;
 import com.chfourie.gwtportletbridge.server.servlet.container.ServletInputStreamImpl;
-import com.chfourie.gwtportletbridge.server.util.URLCodec;
 
 import javax.portlet.ResourceRequest;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 
-import static com.chfourie.gwtportletbridge.client.general.impl.ServeResourceRequestBuilder.REQUEST_DATA_PARAM_NAME;
 import static com.chfourie.gwtportletbridge.client.general.impl.ServeResourceRequestBuilder.CONTENT_TYPE_HEADER;
+import static com.chfourie.gwtportletbridge.client.general.impl.ServeResourceRequestBuilder.REQUEST_DATA_PARAM_NAME;
 
 public class GwtServiceServletRequest extends HttpServletRequestImpl {
   private static final String CHARSET_UTF8 = "UTF-8";
@@ -31,27 +29,29 @@ public class GwtServiceServletRequest extends HttpServletRequestImpl {
 
   /*
    * GWT RPC
-   * We post data to the server as 'application/x-www-form-urlencoded'.  Information that the standard GWT RPC
-   * mechanism send as header values, we send as fields in the message body.  The payload is sent as a field named
+   * We post data to the server as 'application/x-www-form-urlencoded'.
+   * Information that the standard GWT RPC mechanism send as header values, we
+   * send as fields in the message body.  The payload is sent as a field named
    * 'payload'.
    */
   @Override
   public String getHeader(String name) {
     return parameterNames.contains(name)
-        ? URLCodec.decode(getResourceRequest().getParameter(name))
+        ? getResourceRequest().getParameter(name)
         : super.getHeader(name);
   }
 
 
   /*
    * GWT RPC
-   * We post data to the server as 'application/x-www-form-urlencoded'.  Information that the standard GWT RPC
-   * mechanism send as header values, we send as fields in the message body.  The payload is sent as a field named
+   * We post data to the server as 'application/x-www-form-urlencoded'.
+   * Information that the standard GWT RPC mechanism send as header values, we
+   * send as fields in the message body. The payload is sent as a field named
    * 'payload'.
    */
   @Override
   public ServletInputStream getInputStream() throws IOException {
-    String data = URLDecoder.decode("" + getParameter(REQUEST_DATA_PARAM_NAME), CHARSET_UTF8);
+    String data = getParameter(REQUEST_DATA_PARAM_NAME);
     return new ServletInputStreamImpl(new ByteArrayInputStream(data.getBytes(CHARSET_UTF8)));
   }
 
